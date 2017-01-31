d62dc345b55fccf539fec52be82f296f812fa7d6
Chengxu Zhuang <chengxuz@sherlock-ln01.stanford.edu>
2017-01-30 21:14:25 -0800

    Add some parameters

--- cmd_gen_mp4/run_sbatch.sh
+++ cmd_gen_mp4/run_sbatch.sh
@@ -4,9 +4,10 @@
 #    sbatch --job-name=dnhyper${k} script_sbatch.sh ${k}
 #done
 
-sbatch --job-name=dhmons1000 script_sbatch_monserver.sh 1000 exp2
+k=500
+sbatch --job-name=dhmons${k} script_sbatch_monserver.sh ${k} exp3
 sleep 5
 for k in $(seq 1 10)
 do
-    sbatch --job-name=dhmonc1000 script_sbatch_monclient.sh 23333 test_db
+    sbatch --job-name=dhmonc${k} script_sbatch_monclient.sh 23333 test_db
 done
