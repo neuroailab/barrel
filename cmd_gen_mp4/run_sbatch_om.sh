40a11ca15d0b553917eeb46acdc0e3968d85932c
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-02-01 00:14:00 -0500

    runnable on om7

---
+++ cmd_gen_mp4/run_sbatch_om.sh
@@ -0,0 +1,14 @@
+#for k in 100 1000 5000
+#for k in 100 500 1000
+#do
+#    sbatch --job-name=dnhyper${k} script_sbatch.sh ${k}
+#done
+
+#k=500
+#sbatch --job-name=dhmons${k} script_sbatch_monserver.sh ${k} exp4
+#sleep 5
+#for k in $(seq 1 10)
+for k in $(seq 1 20)
+do
+    sbatch --job-name=dhmonc${k} script_sbatch_monclient_om.sh 23333 test_db
+done
