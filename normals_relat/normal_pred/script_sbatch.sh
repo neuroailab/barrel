4afc65325ddd4eda599a8fdb11b13438d1176eeb
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-02-08 23:16:54 -0500

    Openmind7 running minor

--- normals_relat/normal_pred/script_sbatch.sh
+++ normals_relat/normal_pred/script_sbatch.sh
@@ -1,3 +1,4 @@
 #sbatch -J normalnet -o /om/user/chengxuz/slurm_out_all/slurm_normalnet_%j.out ./script_train.sh 1
-sbatch -J normalnet -o /om/user/chengxuz/slurm_out_all/slurm_normalnet_%j.out ./script_train_tfr.sh 1 trainval1_tfr 27017
+#sbatch -J normalnet -o /om/user/chengxuz/slurm_out_all/slurm_normalnet_%j.out ./script_train_tfr.sh 1 trainval1_tfr 27017
 #sbatch -J normalnet -o /om/user/chengxuz/slurm_out_all/slurm_normalnet_%j.out ./script_train_tfr.sh 1 trainval1_tfr_test 27017
+sbatch -J normalnet -o /om/user/chengxuz/slurm_out_all/slurm_normalnet_%j.out ./script_train_tfr.sh 1 trainval1_tfr_bf 27017
