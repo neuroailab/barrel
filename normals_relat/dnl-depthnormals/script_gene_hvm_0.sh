aa91547f323df474feaa13a511ffcff890308a8b
chengxuz@yaminlab1.stanford <chengxuz@stanford.edu>
2017-02-23 15:05:12 -0800

    Minor

--- normals_relat/dnl-depthnormals/script_gene_hvm_0.sh
+++ normals_relat/dnl-depthnormals/script_gene_hvm_0.sh
@@ -1,5 +1,8 @@
 for layer in 1.1 1.2 1.3 2.1 2.2 2.3 2.4 2.5 2.6 3.1 3.2 3.3 3.4 3.5
 do
-    THEANO_FLAGS=device=gpu2 python gene_hvm_response.py --layer ${layer} --network 1 --saveprefix hvm_vgg_layer_
+    THEANO_FLAGS=device=gpu0 python gene_hvm_response.py --layer ${layer} --network 1 --saveprefix hvm_vgg_layer_
+done
+for layer in 1.1 1.2 1.3 2.1 2.2 2.3 2.4 2.5 2.6 3.1 3.2 3.3 3.4 3.5
+do
     python neural_fit_response.py --layer ${layer} --network 1 --loadprefix hvm_vgg_layer_ --saveprefix regress_vgg_layer_
 done
