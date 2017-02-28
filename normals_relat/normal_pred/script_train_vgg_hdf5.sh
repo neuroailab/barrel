e5fc644564dc9062f6e8feb001b7beca0232f29a
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-02-28 04:03:12 -0500

    Train other networks

---
+++ normals_relat/normal_pred/script_train_vgg_hdf5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --gres=gpu:titan-x:1
+#SBATCH --mem=35000
+#SBATCH -t 7-0:0:0
+#SBATCH -c 5
+# Original training script
+source activate env_torch
+source ~/.tunnel
+#python train_normalnet.py --expId trainval_om --seed ${1}
+python train_normalnet.py --expId ${2} --seed ${1} --cacheDirPrefix /om/user/chengxuz --nport ${3} --namefunc normal_vgg16_tfutils --pathconfig normals_config_fcnvgg16.cfg --usehdf5 1 --whichloss ${4} --whichrate ${5}
