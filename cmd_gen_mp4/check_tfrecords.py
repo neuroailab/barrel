6d22ce933764a169efa7bab7eb9d4b840b6eead6
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-03-28 14:36:30 -0400

    Add check file

---
+++ cmd_gen_mp4/check_tfrecords.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+
+savedir = '/om/user/chengxuz/Data/barrel_dataset/tfrecords'
+
+key_list =[
+    u'category',
+    u'scale',
+    u'Data_force',
+    u'Data_normal',
+    u'Data_torque',
+    u'orn',
+    u'position',
+    u'speed'
+    ]
+
+objlen = 25
+for key_now in key_list:
+    checkdir = os.path.join(savedir, key_now)
+
+    for objsta in xrange(0, 9981, objlen):
+        path_now = os.path.join(checkdir, "Data%i_%i.tfrecords" % (objsta, objlen))
+
+        if not os.path.isfile(path_now):
+            print path_now
