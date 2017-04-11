44efb1a5871c5d1e55837b5d4aaecf97eb57efe8
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-04-11 02:02:37 -0400

    Check objects

--- cmd_gen_mp4/script_gendataset.sh
+++ cmd_gen_mp4/script_gendataset.sh
@@ -5,7 +5,9 @@
 
 source activate env_torch_2
 #ssh -f -N -L 22334:localhost:22334 chengxuz@dicarlo5.mit.edu
-python cmd_dataset.py --objsta ${1} --objlen ${2} --bigsamnum ${3} --checkmode 0
+#python cmd_dataset.py --objsta ${1} --objlen ${2} --bigsamnum ${3} --checkmode 0
+# For generating data on 4039 for test
+python cmd_dataset.py --objsta ${1} --objlen ${2} --bigsamnum ${3} --checkmode 0 --savedir /om/user/chengxuz/Data/barrel_dataset/raw_hdf5_test
 
 # Change to write the information
 #python cmd_dataset.py --objsta ${1} --objlen ${2} --bigsamnum ${3} --checkmode 2
