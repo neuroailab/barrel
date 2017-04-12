32a5fbf380b6424c9a610825eb07c32423709168
Chengxu Zhuang <zhuangcx93@gmail.com>
2017-04-11 23:37:07 -0400

    Add tfrecords test

--- normals_relat/other_dataset/scenenet/check_tfrecords.py
+++ normals_relat/other_dataset/scenenet/check_tfrecords.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
 
 #tfrecords_filename = '/home/chengxuz/barrel/barrel_github/dataset/scenenet/tfrecords_depth/data_0.tfrecords'
 #tfrecords_filename = '/home/chengxuz/barrel/barrel_github/dataset/scenenet/tfrecords_ins/data_0.tfrecords'
-tfrecords_filename = '/home/chengxuz/barrel/barrel_github/dataset/scenenet/tfrecords/data_0.tfrecords'
+#tfrecords_filename = '/home/chengxuz/barrel/barrel_github/dataset/scenenet/tfrecords/data_0.tfrecords'
+tfrecords_filename = '/om/user/chengxuz/Data/one_world_dataset/split_scenenet/train_tfrecords/0/normal/data_0.tfrecords'
 reconstructed_images = []
 
 record_iterator = tf.python_io.tf_record_iterator(path=tfrecords_filename)
@@ -23,19 +25,28 @@ for string_record in record_iterator:
     reconstructed_img = img_1d.reshape((240, 320, -1))
     
     reconstructed_images.append(reconstructed_img)
+    #break
 
-img_inter = reconstructed_images[0]
-print(np.max(img_inter))
-print(np.sum(img_inter))
-print(np.min(img_inter))
-print(np.min(img_inter[100][104]))
+img_save_prefix = '/om/user/chengxuz/Data/one_world_dataset/split_scenenet/normal_outs/'
+#img_inter = reconstructed_images[0]
+#img_PIL = Image.fromarray(img_inter)
+#img_PIL.save('normal_test_0.jpg')
+for img_out_indx in xrange(10):
+    img_inter = reconstructed_images[img_out_indx]
+    img_PIL = Image.fromarray(img_inter)
+    img_PIL.save(os.path.join(img_save_prefix, 'normal_test_%i.jpg' % img_out_indx))
+    
+#print(np.max(img_inter))
+#print(np.sum(img_inter))
+#print(np.min(img_inter))
+#print(np.min(img_inter[100][104]))
 
-img_list = [0, 25, 50, 75, 100]
+#img_list = [0, 25, 50, 75, 100]
 #img_filenames = ["/mnt/data2/chengxuz/train/0/0/depth/%i.png" % x for x in img_list]
 #img_filenames = ["/mnt/data2/chengxuz/train/0/0/instance/%i.png" % x for x in img_list]
-img_filenames = ["/mnt/data2/chengxuz/train/0/0/photo/%i.jpg" % x for x in img_list]
+#img_filenames = ["/mnt/data2/chengxuz/train/0/0/photo/%i.jpg" % x for x in img_list]
 
-for img_indx, img_filename in enumerate(img_filenames):
-    recon_img = reconstructed_images[img_indx]
+#for img_indx, img_filename in enumerate(img_filenames):
+#    recon_img = reconstructed_images[img_indx]
 
-    print(np.allclose(recon_img, np.array(Image.open(img_filename))))
+#    print(np.allclose(recon_img, np.array(Image.open(img_filename))))
