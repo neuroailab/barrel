8f470e0c5f96a0920177e32c1fffb69f6015e96e
Chengxu Zhuang <chengxuz@sherlock-ln04.stanford.edu>
2017-05-09 01:35:08 -0700

    Add test dataset generating

--- cmd_gen_mp4/run_sbatch_hdf5.sh
+++ cmd_gen_mp4/run_sbatch_hdf5.sh
@@ -18,6 +18,8 @@ done
 #python cmd_hdf5.py --pathhdf5 /om/user/chengxuz/barrel/hdf5s --pathexe /om/user/chengxuz/barrel/example_build/Constraints/App_TestHinge --fromcfg /om/user/chengxuz/barrel/barrel/cmd_gen_mp4/opt_results/para_ --pathconfig /om/user/chengxuz/barrel/configs --objindx ${1} --spindxlen 1 --scindxlen 1 --oindxsta ${2} --pindxsta ${3}
 #python cmd_hdf5.py --pathhdf5 /om/user/chengxuz/barrel/hdf5s_val --pathexe /om/user/chengxuz/barrel/example_build/Constraints/App_TestHinge --fromcfg /om/user/chengxuz/barrel/barrel/cmd_gen_mp4/opt_results/para_ --pathconfig /om/user/chengxuz/barrel/configs --objindx ${1} --spindxlen 3 --scindxlen 3 --oindxsta ${2} --pindxsta ${3} --generatemode 2 --testmode 2
 
+
+:'
 for objindx in 0 1
 #for objindx in 0
 do
@@ -35,3 +37,17 @@ do
         done
     done
 done
+'
+
+data_path1=/scratch/users/chengxuz/barrel/bullet3/data/teddy2_VHACD_CHs.obj
+data_path2=/scratch/users/chengxuz/barrel/bullet3/data/duck_vhacd.obj
+
+bigsamnum=4
+
+for seed in $(seq 0 250)
+#for seed in 0
+do
+    sbatch script_gentestdataset_sher.sh ${data_path1} ${bigsamnum} teddy_${seed} ${seed}
+    sbatch script_gentestdataset_sher.sh ${data_path2} ${bigsamnum} duck_${seed} ${seed}
+done
+
