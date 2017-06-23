eb773049e14ff6ea699d9fcad49d4a167ee4bf46
Chengxu Zhuang <chengxuz@DN0a221351.SUNet>
2017-06-22 20:34:49 -0700

    Submission to arxiv

---
+++ papers/arxiv_2017/build.sh
@@ -0,0 +1,1 @@
+pdflatex nips2017.tex; pdflatex nips2017.tex; bibtex nips2017; pdflatex nips2017.tex; pdflatex nips2017.tex; bibtex nips2017; pdflatex nips2017.tex; open nips2017.pdf
---
+++ papers/arxiv_2017_2/build_fyp.sh
@@ -0,0 +1,4 @@
+./build_wo.sh supp_main 
+./build_wo.sh fyp_main
+./build_wo.sh fyp_all
+open fyp_all.pdf
